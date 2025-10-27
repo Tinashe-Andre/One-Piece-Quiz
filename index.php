4fbeafea967f9af46157464ba28b827878123d52
Tinashe Andre <tamakwiramiti10@gmail.com>
2025-10-27 15:30:24 +0200

    Adding the progress feature

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-    include("header.html");
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,57 +7,92 @@
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
+    <?php include("header.html"); ?>
     <main>
         <section id="quiz">
             <h1>Test Your One Piece Knowledge!</h1>
             <form action="<?php htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post">
                 <div class="question">
                     <label>Who is the Vice Captin of the Straw Hat Pirates?</label><br>
-                    <input type="radio" name="vice-captin"> Black Leg Sanji <br>
-                    <input type="radio" name="vice-captin"> Dr. Chopper <br>
-                    <input type="radio" name="vice-captin"> Pirate Hunter Zoro <br>
-                    <input type="radio" name="vice-captin"> Cat Burgler Nami <br>
+                    <input type="radio" name="vice-captin" value="Sanji"> Black Leg Sanji <br>
+                    <input type="radio" name="vice-captin" value="Chopper"> Dr. Chopper <br>
+                    <input type="radio" name="vice-captin" value="Zoro"> Pirate Hunter Zoro <br>
+                    <input type="radio" name="vice-captin" value="Nami"> Cat Burgler Nami <br>
                 </div>
                 <div class="question">
                     <label>Who is the Navigator of the Straw Hat Pirates?</label><br>
-                    <input type="radio" name="navigator"> Black Leg Sanji <br>
-                    <input type="radio" name="navigator"> Dr. Chopper <br>
-                    <input type="radio" name="navigator"> Pirate Hunter Zoro <br>
-                    <input type="radio" name="navigator"> Cat Burgler Nami <br>
+                    <input type="radio" name="navigator" value="Sanji"> Black Leg Sanji <br>
+                    <input type="radio" name="navigator" value="Chopper"> Dr. Chopper <br>
+                    <input type="radio" name="navigator" value="Zoro"> Pirate Hunter Zoro <br>
+                    <input type="radio" name="navigator" value="Nami"> Cat Burgler Nami <br>
                 </div>
                 <div class="question">
                     <label>Who has the highest bounty in the Straw Hat Pirates?</label><br>
-                    <input type="radio" name="bounty"> Pirate Hunter Zoro <br>
-                    <input type="radio" name="bounty"> Monkey D. Luffy <br>
-                    <input type="radio" name="bounty">  First Son of the Sea Jinbe<br>
-                    <input type="radio" name="bounty"> Cyborg Franky <br>
+                    <input type="radio" name="bounty" value="Zoro"> Pirate Hunter Zoro <br>
+                    <input type="radio" name="bounty" value="Luffy"> Monkey D. Luffy <br>
+                    <input type="radio" name="bounty" value="Jinbe">  First Son of the Sea Jinbe<br>
+                    <input type="radio" name="bounty" value="Franky"> Cyborg Franky <br>
                 </div>
                 <div class="question">
                     <label>Who is the cutest Straw Hat Pirate?</label><br>
-                    <input type="radio" name="cutest"> Robin <br>
-                    <input type="radio" name="cutest"> Nami <br>
-                    <input type="radio" name="cutest"> Ussop <br>
-                    <input type="radio" name="cutest"> CChopper <br>
+                    <input type="radio" name="cutest" value="Robin"> Robin <br>
+                    <input type="radio" name="cutest" value="Nami"> Nami <br>
+                    <input type="radio" name="cutest" value="Ussop"> Ussop <br>
+                    <input type="radio" name="cutest" value="Chopper"> Chopper <br>
                 </div>
                 <input class="button" type="submit" name="check" value="Check Answers">
             </form>
             <?php
-                $answer = null;
+                $points = 0;
+                $message = "";
 
                 if(isset($_POST["check"])){
-                    
+                    $vice_cap_ans = $_POST["vice-captin"];
+                    $navigator_ans = $_POST["navigator"];
+                    $bounty_ans = $_POST["bounty"];
+                    $cutest_ans = $_POST["cutest"];
+
+                    if($vice_cap_ans == "Zoro"){
+                        $points++;
+                        $message = "Zoro is the Vice Captin!";
+                    } else{
+                        $message = "{$vice_cap_ans} is NOT the Vice Captin :(";
+                    }
+
+                    if($navigator_ans == "Nami"){
+                        $points++;
+                        $message = "Nami is the Navigator!";
+                    } else{
+                        $message = "{$vice_cap_ans} is NOT the Navigator :(";
+                    }
+
+                    if($navigator_ans == "Luffy"){
+                        $points++;
+                        $message = "Luffy does have the highest bounty!";
+                    } else{
+                        $message = "{$vice_cap_ans} does NOT the highest bounty :(";
+                    }
+
+                    if($navigator_ans == "Chopper"){
+                        $points++;
+                        $message = "Chopper is the cutest of course";
+                    } else{
+                        $message = "{$vice_cap_ans} is not the cutest :(";
+                    }
                 }
             ?>
         </section>
         <section id="progress">
             <h1>Progress</h1>
+            <div class="progress-board">
+                <p><?php echo $message?></p>
+                <p>Score: <?php echo $points?></p>
+            </div>
         </section>
         <section id="highscore">
             <h1>High Score</h1>
         </section>
     </main>
+    <?php include("footer.html"); ?>
 </body>
 </html>
-<?php
-    include("footer.html");
-?>
